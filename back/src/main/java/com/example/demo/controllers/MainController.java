a6310fac30ca9f119208a4441cce68dbb148d1d7
rayenrejeb <rayen.rejeb@gmail.com>
2018-10-25 23:12:06 +0100

    Controller added

---
+++ back/src/main/java/com/example/demo/controllers/MainController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = {"${allowedUrl}"}, maxAge = 3600)
+@RestController
+@RequestMapping("/api")
+public class MainController {
+
+    @GetMapping("/count")
+    public int getNumber(){
+        return 0;
+    }
+}
